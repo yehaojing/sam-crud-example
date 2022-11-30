e64cd510f57247ba430dc65f4b19a5cf2a168cb4
William Ye <yehaojing@protonmail.com>
2022-11-30 14:41:47 +1100

    added DDB table + Lambda powertools resolver

--- lambda/app.py
+++ lambda/app.py
@@ -1,42 +1,76 @@
-import json
+import boto3
+import os
+from aws_lambda_powertools.event_handler import APIGatewayRestResolver
+import uuid
 
-# import requests
-
+ddb = boto3.resource("dynamodb")
+table = ddb.Table(os.environ['ddb_table_name'])
+app = APIGatewayRestResolver()
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+    return app.resolve(event, context)
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
+@app.post("/user")
+def post_user():
+    body = app.current_event.json_body
+    body["id"] = str(uuid.uuid1())
+    table.put_item(
+        Item=body
+    )
 
     return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
+        "statusCode": 202,
+        "message": "New user created.",
+        "user": body
+    }
+
+@app.get("/user/<id>")
+def get_user(id):
+    resp = table.get_item(
+        Key={
+            "id": id
+        }
+    )
+
+    if "Item" in resp.keys():
+        return { "statusCode": 200, "user": resp["Item"] }
+    else:
+        return { "statusCode": 404, "message": "User not found."}
+
+@app.get("/user")
+def get_user():
+    resp = table.scan()
+
+    if "Items" in resp.keys():
+        return { "statusCode": 200, "users": resp["Items"] }
+    else:
+        return { "statusCode": 404, "message": "No users found."}
+
+@app.put("/user/<id>")
+def update_user(id):
+    body = app.current_event.json_body
+    body.update(
+        {"id":id}
+    )
+    table.put_item(
+        Item=body
+    )
+
+    return {
+        "statusCode": 202,
+        "message": "User updated.",
+        "user": body
+    }
+
+@app.delete("/user/<id>")
+def delete_user(id):
+    resp = table.delete_item(
+        Key={
+            "id": id
+        }
+    )
+
+    return {
+        "statusCode": 202,
+        "message": "User deleted."
     }
